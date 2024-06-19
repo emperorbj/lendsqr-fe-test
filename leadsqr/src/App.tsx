159e4c34dd2be4ec4bf28a87ef4d20fa76e0970b
emperorbj <cwaku96@gmail.com>
2024-06-19 13:08:49 +0100

    added code splitting to the components

--- leadsqr/src/App.tsx
+++ leadsqr/src/App.tsx
@@ -1,25 +1,29 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Login from './pages/Login';
-import Dashboard from './pages/Dashboard';
-import Test from './components/Test'
-import UsersDetailsPage from './pages/UsersDetailsPage';
+import React, { Suspense, lazy } from 'react';
+import Spinner from './components/Spinner';
+// import Login from './pages/Login';
+// import Dashboard from './pages/Dashboard';
+// import UsersDetailsPage from './pages/UsersDetailsPage';
 
-
+const Login = lazy(() => import('./pages/Login'));
+const Dashboard = lazy(() => import('./pages/Dashboard'));
+const UsersDetailsPage = lazy(() => import('./pages/UsersDetailsPage'));
 
 function App() {
 
 
   return (
     <div>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Login/>}/>
-        <Route path='/dashboard' element={<Dashboard/>}/>
-        <Route path='/test' element={<Test/>}/>
-        <Route path='/users/:id' element={<UsersDetailsPage/>}/>
-      </Routes>
-  </BrowserRouter>
-  </div>
+      <BrowserRouter>
+        <Suspense fallback={<Spinner />}>
+          <Routes>
+            <Route path='/' element={<Login />} />
+            <Route path='/dashboard' element={<Dashboard />} />
+            <Route path='/users/:id' element={<UsersDetailsPage />} />
+          </Routes>
+        </Suspense>
+      </BrowserRouter>
+    </div>
   )
 }
 
